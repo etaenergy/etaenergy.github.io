a5bfdee3ce82cc47aca099a22342382e8b6cd5a0
etaenergy <etaecom97@gmail.com>
2025-02-13 12:32:40 +0400

    fsatgeythter

--- index.38d394c2.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function toggleServiceSection(sectionId) {
-  const section = document.getElementById(sectionId);
-  if (section) {
-      section.style.display = section.style.display === 'none' ? 'block' : 'none';
-  } else {
-      console.error('Section not found:', sectionId);
-  }
-}
