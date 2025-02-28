1d5da1a62902cb364cb371c400086c164a104d98
etaenergy <etaecom97@gmail.com>
2025-02-28 17:07:47 +0400

    r67i9euu

--- about.8f3927b6.js
+++
@@ -1,2 +0,0 @@
-gsap.timeline({defaults:{duration:1}}).to("body",{opacity:1,ease:"Power1.easeInOut",delay:.5}).to(".nav_component",{opacity:1,y:0,stagger:.1,ease:"Power1.easeInOut"},"-=.6").fromTo(".about-header-title",{scale:0,opacity:0},{opacity:1,scale:1,y:0,ease:"Power1.easeInOut"},"-=0.3");
-//# sourceMappingURL=about.8f3927b6.js.map
