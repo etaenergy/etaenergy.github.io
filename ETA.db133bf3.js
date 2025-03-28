5467250ff169452e05c957df6d8c86f7252113d0
etaenergy <etaecom97@gmail.com>
2025-03-28 10:17:36 +0400

    ghdfh

--- ETA.db133bf3.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function(){function n(n,i){i.is(":checked")?$("#"+n).slideDown():$("#"+n).slideUp()}$("#installation").on("change",function(){n("installation-details",$(this))}),$("#modernization").on("change",function(){n("modernization-details",$(this))}),$("#maintenance").on("change",function(){n("maintenance-details",$(this))})});
-//# sourceMappingURL=ETA.db133bf3.js.map
