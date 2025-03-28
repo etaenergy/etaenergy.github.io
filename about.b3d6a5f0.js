ac8359cf1f98410a94b1add626b04b0db625a894
etaenergy <etaecom97@gmail.com>
2025-03-28 09:58:34 +0400

    eyeruye

--- about.b3d6a5f0.js
+++
@@ -1,1 +0,0 @@
-var tmlabt=gsap.timeline({defaults:{duration:1}});tmlabt.to("body",{opacity:1,ease:"Power1.easeInOut",delay:.5}).to(".nav_component",{opacity:1,y:0,stagger:.1,ease:"Power1.easeInOut"},"-=.6").fromTo(".about-header-title",{scale:0,opacity:0},{opacity:1,scale:1,y:0,ease:"Power1.easeInOut"},"-=0.3");
