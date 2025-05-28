300ac4a15937bc1def716124cdfd0fc314d2d5b3
etaenergy <etaecom97@gmail.com>
2025-05-28 13:26:10 +0400

    jhyyfymk

---
+++ newinstallation.ad7d65a4.js
@@ -0,0 +1,16 @@
+var $d1fab0557ff1ae73$exports = {};
+$(document).ready(function() {
+    $(".fab").click(function() {
+        $(this).find("i").toggleClass("fa-times fa-comment");
+        $(".floatingMenu").toggleClass("open");
+        $(".floatingMenu li").each(function(i, li) {
+            var $list = $(".floatingMenu");
+            $list.queue(function() {
+                $(li).toggleClass("visible");
+                $list.dequeue();
+            }).delay(50);
+        });
+    });
+});
+
+
