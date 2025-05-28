8c23d7fe4f8c91cabdd629431863f5e33d70ad27
etaenergy <etaecom97@gmail.com>
2025-05-28 12:11:10 +0400

    sdgadgdrgh

---
+++ newinstallation.301b5088.js
@@ -0,0 +1,57 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   console.log("Back button script loaded");
+//   const backBtn = document.querySelector("[data-back-btn]");
+//   console.log("Back button found:", backBtn);
+//   if (backBtn) {
+//     backBtn.addEventListener("click", function (event) {
+//       event.preventDefault();
+//       document.body.classList.add("fade-out");
+//       setTimeout(() => {
+//         history.back();
+//       }, 500); // Match your CSS transition
+//     });
+//   }
+// });
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("Back button script loaded");
+//   const backBtn = document.querySelector("[data-back-btn]");
+//   console.log("Back button found:", backBtn);
+//   if (backBtn) {
+//     backBtn.addEventListener("click", function (event) {
+//       event.preventDefault();
+//       // Apply fade-out transition
+//       // document.body.classList.add("fade-out");
+//       // Delay going back until after transition
+//       setTimeout(() => {
+//         history.back();
+//       }, 500); // Match with CSS transition duration
+//     });
+//   }
+//   // // Ensure fade-out is removed when page is shown (from bfcache or reload)
+//   // window.addEventListener("pageshow", function (event) {
+//   //   console.log("Page show, persisted:", event.persisted);
+//   //   document.body.classList.remove("fade-out");
+//   //   // Optional: fix blank page in iOS/Safari by forcing redraw
+//   //   if (event.persisted) {
+//   //     document.body.style.display = "none";
+//   //     document.body.offsetHeight; // force reflow
+//   //     document.body.style.display = "";
+//   //   }
+//   // });
+//   // // Also remove fade-out in normal load
+//   // window.addEventListener("load", () => {
+//   //   document.body.classList.remove("fade-out");
+//   // });
+// });
+document.addEventListener("DOMContentLoaded", function() {
+    const burger = document.querySelector(".nav_menu-lottie-hamburgerr");
+    if (burger && !burger.querySelector("span")) {
+        const span = document.createElement("span");
+        burger.appendChild(span);
+    }
+    burger.addEventListener("click", function() {
+        burger.classList.toggle("active");
+    });
+});
+
+//# sourceMappingURL=newinstallation.301b5088.js.map
